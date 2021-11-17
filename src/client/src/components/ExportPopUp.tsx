79d4ece97981cac14ec189dcfdfb069a4c51d19d
santi-g-s <santiago@garciasantos.org>
2021-11-17 16:06:16 -0500

    Completed PopUp UI
    Added toggles and export option buttons

--- src/client/src/components/ExportPopUp.tsx
+++ src/client/src/components/ExportPopUp.tsx
@@ -1,11 +1,16 @@
+import { before } from 'lodash';
 import React, { useState } from 'react';
 import ReactModal from 'react-modal';
 import styled from 'styled-components';
 
+// *Styling*
+
+// Rounded Button
 const Button = styled.a`
   background: transparent;
   border-radius: 50px;
   font-weight: bold;
+  font-size: 14px;
   background-color: #1aabb8;
   color: white;
   padding: 0.5em 1em;
@@ -20,6 +25,70 @@ const Button = styled.a`
   }
 `;
 
+const CancelButton = styled(Button)`
+  background-color: #acacac;
+  &:hover {
+    background-color: #efefef;
+    color: white;
+  }
+`;
+
+// Toggle components
+const Input = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #dbdbdb;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  &:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+`;
+
+const RoundedSlider = styled(Slider)`
+  border-radius: 34px;
+  &:before {
+    border-radius: 50%;
+  }
+`;
+
+const Switch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  & ${Input}:checked + ${Slider} {
+    background-color: #1aabb8;
+  }
+  & ${Input}:focus + ${Slider} {
+    box-shadow: 0 0 1px #1aabb8;
+  }
+  & ${Input}:checked + ${Slider}:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+`;
+
+// Modal Styling
 const customModalStyles = {
   overlay: {
     position: 'fixed',
@@ -49,6 +118,8 @@ const customModalStyles = {
   },
 };
 
+// *PopUp Component*
+
 function ExportPopUp(props) {
   let subtitle;
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -62,17 +133,14 @@ function ExportPopUp(props) {
   function handleClick() {
     setInclDet(!inclDet);
     console.log(inclDet);
-    let val: string = inclDet ? 'fas fa-check-circle' : 'fa fa-check-circle';
-    document.getElementById('detailedToggle').setAttribute('class', val);
+    let val: string = inclDet ? 'fas fa-check-circle' : 'far fa-check-circle';
+    document.getElementById('detailedToggle').setAttribute('className', val);
   }
 
   return (
     <div>
       <Button onClick={toggleModalIsOpen}>
-        <i
-          className="fas fa-file-download fa-fw"
-          style={{ color: 'white' }}
-        ></i>{' '}
+        <i className="fas fa-share-square fa-fw" style={{ color: 'white' }}></i>{' '}
         {props.title}
       </Button>
       <ReactModal
@@ -80,52 +148,161 @@ function ExportPopUp(props) {
         style={customModalStyles}
         contentLabel="Export Modal"
       >
+        {/* Vertical Stack of items */}
         <div
           style={{
             display: 'flex',
-            flexDirection: 'row',
+            flexDirection: 'column',
             justifyContent: 'space-between',
-            alignItems: 'center',
-            fontSize: '24px',
-            color: '#585858',
-            width: '600px',
+            gap: '40px',
+            padding: '20px',
           }}
         >
-          <h1 style={{ fontWeight: 'bold' }}>Export Your Plan</h1>
-          <Button
-            onClick={toggleModalIsOpen}
-            style={{ fontSize: '14px', backgroundColor: '#ACACAC' }}
+          {/* Header */}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
           >
-            <i className="fas fa-times fa-fw" style={{ color: 'white' }}></i>{' '}
-            Close
-          </Button>
-        </div>
-        <div>Download a summary of your plan</div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            color: '#585858',
-            width: '600px',
-          }}
-        >
-          <div style={{ fontWeight: 'bold' }}>
-            Would you like to include a detailed copy?
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                fontSize: '24px',
+                color: '#585858',
+                width: '600px',
+              }}
+            >
+              <h1 style={{ fontWeight: 'bold' }}>Export Your Plan</h1>
+              <CancelButton onClick={toggleModalIsOpen}>
+                <i
+                  className="fas fa-times fa-fw"
+                  style={{ color: 'white' }}
+                ></i>{' '}
+                Cancel
+              </CancelButton>
+            </div>
+            <div>Download a summary of your plan</div>
           </div>
-          <div>
-            <div onClick={handleClick}>
-              <i
-                id="detailedToggle"
-                className="fas fa-check-circle"
-                style={{ color: '#1aabb8' }}
-              ></i>
+
+          {/* Doctor's copy */}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              gap: '10px',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                color: '#585858',
+                width: '600px',
+              }}
+            >
+              <div style={{ fontWeight: 'bold' }}>
+                Would you like to include a doctor's copy?
+              </div>
+              <div>
+                <Switch>
+                  <Input type="checkbox" />
+                  <RoundedSlider></RoundedSlider>
+                </Switch>
+              </div>
+            </div>
+            <div
+              style={{
+                width: '600px',
+              }}
+            >
+              The doctor’s copy is a Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed eu justo eros. Nam vitae orci augue.
             </div>
           </div>
-        </div>
-        <div style={{ fontWeight: 'bold' }}>
-          How would you like to receive your plan?
+
+          {/* Detailed copy */}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              gap: '10px',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                color: '#585858',
+                width: '600px',
+              }}
+            >
+              <div style={{ fontWeight: 'bold' }}>
+                Would you like to include a detailed copy?
+              </div>
+              <div>
+                <Switch>
+                  <Input type="checkbox" />
+                  <RoundedSlider></RoundedSlider>
+                </Switch>
+              </div>
+            </div>
+            <div
+              style={{
+                width: '600px',
+              }}
+            >
+              The detailed copy is a Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed eu justo eros. Nam vitae orci augue.
+            </div>
+          </div>
+
+          {/* Export Options */}
+          <div style={{ fontWeight: 'bold', color: '#585858' }}>
+            How would you like to receive your plan?
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              alignItems: 'center',
+              color: '#585858',
+              width: '600px',
+            }}
+          >
+            <Button onClick={toggleModalIsOpen}>
+              <i
+                className="fas fa-file-download fa-fw"
+                style={{ color: 'white' }}
+              ></i>{' '}
+              Download
+            </Button>
+            <Button onClick={toggleModalIsOpen}>
+              <i
+                className="fas fa-envelope fa-fw"
+                style={{ color: 'white' }}
+              ></i>{' '}
+              Email
+            </Button>
+            <Button onClick={toggleModalIsOpen}>
+              <i
+                className="fas fa-comment fa-fw"
+                style={{ color: 'white' }}
+              ></i>{' '}
+              Text
+            </Button>
+          </div>
         </div>
       </ReactModal>
     </div>
