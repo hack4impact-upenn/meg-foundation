fc0618c4108a4ca63e59eea04d0b9311511cb330
danielbarra24 <72272441+danielbarra24@users.noreply.github.com>
2021-12-01 00:54:41 -0500

    twilio api updates to reproduce auth error

--- src/routes/twilio.api.ts
+++ src/routes/twilio.api.ts
@@ -3,42 +3,53 @@ import auth from '../middleware/auth';
 import errorHandler from './error';
 import { Message, IMessage } from '../models/message.model';
 import TaskRouterCapability from 'twilio/lib/jwt/taskrouter/TaskRouterCapability';
+import twilio = require('twilio');
 
-const twilio = require('twilio');
 const router = express.Router();
 
 const accountSid = 'AC378e250f292f5825ca224d55a9fb0bf9';
 const authToken = '7adedf4fbe26085309b26f19709595cc';
-const client = new twilio(accountSid, authToken);
+const client = twilio(accountSid, authToken);
 
 router.post('/sendMessage', async (req, res) => {
-  const { content } = req.body;
-  const { sender } = req.body;
-  const { recipient } = req.body;
+  // const { content } = req.body;
+  // const { sender } = req.body;
+  // const { recipient } = req.body;
+  try {
+    const content = 'hello, this is a test';
+    const sender = '+17404869699';
+    const recipient = '+14086637543';
 
-  console.log('hello');
+    console.log('here1');
 
-  twilio.messages.create({
-    body: content,
-    from: sender,
-    to: recipient,
-  });
+    await client.messages.create({
+      body: content,
+      from: sender,
+      to: recipient,
+    });
 
-  const outgoingMessage = new Message({
-    body: content,
-    from: sender,
-    to: recipient,
-  });
+    console.log('here2');
 
-  outgoingMessage
-    .save()
-    .then(() => {
-      res.status(200).send({
-        success: true,
-        msg: outgoingMessage,
-      });
-    })
-    .catch((err) => console.log(err));
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // const outgoingMessage = new Message({
+  //   body: content,
+  //   from: sender,
+  //   to: recipient,
+  // });
+
+  // outgoingMessage
+  //   .save()
+  //   .then(() => {
+  //     res.status(200).send({
+  //       success: true,
+  //       msg: outgoingMessage,
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
 export default router;
