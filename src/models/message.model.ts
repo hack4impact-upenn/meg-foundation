31196aeb71145129bcdebeebbfa7901ce4385b8e
Rose Wang <rosewang01@gmail.com>
2021-12-05 22:53:42 -0500

    Added Twilio API Routing

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 const { Schema } = mongoose;
 
 interface IMessage extends mongoose.Document {
--- src/routes/twilio.api.ts
+++ src/routes/twilio.api.ts
@@ -1,15 +1,40 @@
-import express from 'express';
-import auth from '../middleware/auth';
-import errorHandler from './error';
-import { Message, IMessage } from '../models/message.model';
+const express = require('express');
+const { Message, IMessage } = require('../models/message.model.ts');
 
 const twilio = require('twilio');
+const router = express.Router();
 
 const accountSid = 'AC378e250f292f5825ca224d55a9fb0bf9';
 const authToken = '7adedf4fbe26085309b26f19709595cc';
 const client = new twilio(accountSid, authToken);
 
-client.messages
+router.post('/sendMessage', function (req, res) {
+  const content = req.body.content;
+  const sender = req.body.sender;
+  const recipient = req.body.recipient;
+
+  twilio.messages.create({
+    body: content,
+    from: sender,
+    to: recipient,
+  });
+
+  const outgoingMessage = new Message({
+    body: content,
+    from: sender,
+    to: recipient,
+  });
+
+  outgoingMessage.save().then(() => {
+    res.status(200).send({
+      success: true,
+      msg: outgoingMessage,
+    });
+  });
+});
+
+/**
+ * client.messages
   .create(
     new Message({
       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
@@ -18,3 +43,4 @@ client.messages
     })
   )
   .then((message) => console.log(message));
+ */
