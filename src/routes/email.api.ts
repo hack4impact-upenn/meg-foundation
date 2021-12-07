b8e5247369c28f193bb3462ff245e86c0a6ecbf8
icpedroza <ipedroza@wharton.upenn.edu>
2021-12-06 21:18:43 -0500

    Fixed bug

--- src/routes/email.api.ts
+++ src/routes/email.api.ts
@@ -20,7 +20,7 @@ async function sendMail() {
     service: 'gmail',
     auth: {
       type: 'OAuth2',
-      user: process.env.gitMAIL_USERNAME,
+      user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD,
       clientId: process.env.OAUTH_CLIENTID,
       clientSecret: process.env.OAUTH_CLIENT_SECRET,
